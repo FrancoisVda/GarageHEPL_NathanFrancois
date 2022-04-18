885b3e0362d2e93319187ac6ca009a4b8e9370e0
nathanluc08 <Bigado0810200154>
2022-04-18 16:20:32 +0200

    Merge remote-tracking branch 'github/master'

--- src/GarageHEPL_NathanFrancois/NouveauTravail.java
+++ src/GarageHEPL_NathanFrancois/NouveauTravail.java
@@ -1,4 +1,18 @@
 package GarageHEPL_NathanFrancois;
 
+import javax.swing.*;
+
 public class NouveauTravail {
+    private JPanel panel1;
+    private JTextField TF_TypeVoiture;
+    private JCheckBox CB_PlaqueBelge;
+    private JCheckBox CB_New;
+    private JComboBox CB_Proprietaire;
+    private JRadioButton RB_Entretien;
+    private JRadioButton RB_Reparation;
+    private JComboBox CB_TypeTravail;
+    private JTextField TF_Instruction;
+    private JButton B_OK;
+    private JButton B_Annuler;
+    private JTextField TF_Immatriculation;
 }
---
+++ src/GarageHEPL_NathanFrancois/Policlinique.java
@@ -0,0 +1,19 @@
+package GarageHEPL_NathanFrancois;
+
+import javax.swing.*;
+
+public class Policlinique {
+    private JTextField TF_Pont1;
+    private JTextField TF_Pont2;
+    private JTextField TF_Pont3;
+    private JTextField TF_Sol;
+    private JTextField TF_Divers;
+    private JTextPane TP_Image;
+    private JTextField TF_BureauClient;
+    private JTextField TF_BureauCompta;
+    private JCheckBox CB_Patron;
+    private JRadioButton RB_Absent;
+    private JCheckBox CB_Pause;
+    private JRadioButton RB_Present;
+    private JLabel Atelier;
+}
